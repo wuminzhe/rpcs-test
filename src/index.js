b000830f74e41c0528d918bec67ba79193670438
Aki Wu <wuminzhe@gmail.com>
2024-04-15 11:59:50 +0800

    add moonbeams

--- src/index.js
+++ src/index.js
@@ -17,12 +17,17 @@ const App = () => {
   const pangolinRpcUrls = [
     "https://pangolin-rpc.darwinia.network",
   ];
+  const moonbeamRpcUrls = [
+    "https://rpc.api.moonbeam.network",
+    "https://moonbeam-rpc.publicnode.com"
+  ];
 
   return (
     <div class="antialiased bg-gray-100 text-gray-600 flex flex-col justify-center h-screen">
       <UrlsTestPanel title="DARWINIA" rpcUrls={darwiniaRpcUrls} concurrency="20" />
       <UrlsTestPanel title="CRAB" rpcUrls={crabRpcUrls} concurrency="20" />
       <UrlsTestPanel title="PANGOLIN" rpcUrls={pangolinRpcUrls} concurrency="10" />
+      <UrlsTestPanel title="MOONBEAM" rpcUrls={moonbeamRpcUrls} concurrency="20" />
     </div>
   );
 };
