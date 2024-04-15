52f072f874889d27d634d2d183459142af77d58f
Aki Wu <wuminzhe@gmail.com>
2024-04-15 15:53:43 +0800

    add my rpcs

--- src/index.js
+++ src/index.js
@@ -21,6 +21,13 @@ const App = () => {
     "https://rpc.api.moonbeam.network",
     "https://moonbeam-rpc.publicnode.com"
   ];
+  const myDarwiniaRpcUrls = [
+    "https://cors.kahub.in/http://38.242.135.236:6644"
+  ]
+
+  const myCrabRpcUrls = [
+    "https://cors.kahub.in/http://38.242.135.236:9944",
+  ]
 
   return (
     <div class="antialiased bg-gray-100 text-gray-600 flex flex-col justify-center h-screen">
@@ -28,6 +35,8 @@ const App = () => {
       <UrlsTestPanel title="CRAB" rpcUrls={crabRpcUrls} concurrency="20" />
       <UrlsTestPanel title="PANGOLIN" rpcUrls={pangolinRpcUrls} concurrency="10" />
       <UrlsTestPanel title="MOONBEAM" rpcUrls={moonbeamRpcUrls} concurrency="20" />
+      <UrlsTestPanel title="MY DARWINIA RPCS" rpcUrls={myDarwiniaRpcUrls} concurrency="2" />
+      <UrlsTestPanel title="MY CRAB RPCS" rpcUrls={myCrabRpcUrls} concurrency="2" />
     </div>
   );
 };
